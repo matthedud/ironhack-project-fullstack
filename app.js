98194bed5ccdbe315b8b9df26fb60208473ba986
JoxSalera <97227244+JoxSalera@users.noreply.github.com>
2022-03-28 15:21:35 +0200

    auth

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-require('dotenv/config');
+require("dotenv/config");
 
-require('./db');
+require("./db");
 
-const express = require('express');
+const express = require("express");
 
-const hbs = require('hbs');
+const hbs = require("hbs");
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
 
-require('./config/session.config')(app);
+require("./config/session.config")(app);
 
 // default value for title local
-const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = "lab-express-basic-auth";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-const authRouter = require('./routes/auth');
-app.use('/', index);
-app.use('/auth/', authRouter);
+const index = require("./routes/index");
+const authRouter = require("./routes/auth.routes");
+app.use("/", index);
+app.use("/auth/", authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+require("./error-handling")(app);
 
 module.exports = app;
-
