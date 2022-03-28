2614aa9d75293e10f0a6447357867864005e8d30
JoxSalera <97227244+JoxSalera@users.noreply.github.com>
2022-03-28 17:26:27 +0200

    auth done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,7 +9,14 @@ const saltRounds = 10;
 //////// SIGN UP /////////
 router.post("/signup", async (req, res, next) => {
   try {
-    const { username, email, password } = await req.body;
+    const { username, email, password } = req.body;
+
+    bcryptjs
+      .genSalt(saltRounds)
+      .then((salt) => bcryptjs.hash(password, salt))
+      .then((hashedPassword) => {
+        console.log(`Password hash: ${hashedPassword}`);
+      });
 
     // make sure users fill all mandatory fields:
     if (!username || !email || !password) {
@@ -20,7 +27,7 @@ router.post("/signup", async (req, res, next) => {
       return;
     } else {
       const userNameExist = await userModel.findOne({ username });
-      if (userNameExist === true) {
+      if (userNameExist) {
         res.render("auth/signup", {
           errorMessage: "Oups this name is already taken :(",
         });
@@ -41,54 +48,33 @@ router.post("/signup", async (req, res, next) => {
 router.get("/login", (req, res) => res.render("auth/login"));
 
 // POST login route ==> to process form data
-router.post("/login", (req, res, next) => {
-  const { email, password } = req.body;
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
 
-  if (email === "" || password === "") {
-    res.render("auth/login", {
-      errorMessage: "Please enter both, email and password to login.",
-    });
-    return;
-  }
+    bcryptjs
+      .genSalt(saltRounds)
+      .then((salt) => bcryptjs.hash(password, salt))
+      .then((hashedPassword) => {
+        console.log(`Password hash: ${hashedPassword}`);
+      });
 
-  User.findOne({ email }) // <== check if there's user with the provided email
-    .then((user) => {
-      // <== "user" here is just a placeholder and represents the response from the DB
-      if (!user) {
-        // <== if there's no user with provided email, notify the user who is trying to login
+    if (!username || !password) {
+      res.render("auth/login", {
+        errorMessage: "Please enter both, username and password to login.",
+      });
+      return;
+    } else {
+      const userNameExist = await userModel.findOne({ username });
+      if (userNameExist) {
         res.render("auth/login", {
-          errorMessage: "Email is not registered. Try with other email.",
+          errorMessage: "Oups this name is already taken :(",
         });
-        return;
       }
-
-      // if there's a user, compare provided password
-      // with the hashed password saved in the database
-      else if (bcryptjs.compareSync(password, user.passwordHash)) {
-        // if the two passwords match, render the user-profile.hbs and
-        //                   pass the user object to this view
-        //                                 |
-        //                                 V
-        res.render("users/user-profile", { user });
-      } else {
-        // if the two passwords DON'T match, render the login form again
-        // and send the error message to the user
-        res.render("auth/login", { errorMessage: "Incorrect password." });
-      }
-    })
-    .catch((error) => next(error));
+    }
+  } catch {
+    next(error);
+  }
 });
-
-// router.post("/login", (req, res, next) => {
-//   const { email, username, password } = req.body;
-
-//   bcryptjs
-//     .genSalt(saltRounds)
-//     .then((salt) => bcryptjs.hash(password, salt))
-//     .then((hashedPassword) => {
-//       console.log(`Password hash: ${hashedPassword}`);
-//     })
-//     .catch((error) => next(error));
-// });
 
 module.exports = router;
