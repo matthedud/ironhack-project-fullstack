8c07cba716472af9df5443bbdee2f5fbb2d59e27
bodywosh <bodywashtv@gmail.com>
2022-03-28 11:02:13 +0200

    matrix map

--- models/Map.model.js
+++ models/Map.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const mapSchema = new Schema({
   cells: {
-    type: [Number],
+    type: [[Number]],
     unique: true
   },
 });
