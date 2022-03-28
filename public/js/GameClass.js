3d6e088423c7e93ad2b9f5813f8aac0039a6a799
Matt K <matthedud@hotmail.com>
2022-03-28 17:42:37 +0200

    THe Map is Drawn

--- public/js/GameClass.js
+++ public/js/GameClass.js
@@ -11,7 +11,6 @@ cellheight = canvasHeight /  heigthCellNumber
 class Game {
   constructor(grid2D = [], player=null) {
     this.grid2D = grid2D
-    this.scale = 0.3
     this.gameInterval = null // for player movemetn refresh rate
     this.frameRate = 500
     // this.chronometer = new Chronometer()
@@ -19,43 +18,49 @@ class Game {
     this.player = player
   }
   drawMaze() {
+    const xOffset = Math.floor(this.player.position.x)
+    const yOffset = Math.floor(this.player.position.y)
     for (let y = 0; y < heigthCellNumber; y++) {
       for (let x = 0 ; x < widthCellNumber; x++) {
-        const cellInd = x + this.player.position.x
-        const lineInd = x + this.player.position.y
-        this.drawCell(this.grid2D[lineInd][cellInd], x, y)
+        const cellInd = x + xOffset
+        const lineInd = y + yOffset
+        this.drawCell(cellInd, lineInd, x, y)
       }
     }
-    // this.player.draw()
+    this.player.draw()
     // this.bullets.forEach((bullet) => {
     //   bullet.draw(this.xOffset, this.scale)
     // })
   }
-
-  drawCell(cell, x, y) {
-    ctx.fillStyle = cell ? colors.minimapWall : colors.minimapFloor
-    ctx.beginPath()
-    ctx.fillRect(x, y, this.cellWidth, this.cellheight)
-  }
+	drawCell(cellInd, lineInd, canvasIndX, canvasIndY) {
+		ctx.fillStyle = this.isWall(cellInd, lineInd) ? colors.wall : colors.floor
+		const x =
+    canvasIndX * cellWidth +
+			canvasWidth / 2 -
+			(canvasWidth) / 2
+		const y = canvasIndY * cellheight
+		ctx.beginPath()
+		ctx.fillRect(x, y, cellWidth, cellheight)
+    console.log({cellInd, lineInd})
+	}
 
   isWall(x, y) {
-    const cellIndex = Math.floor(x / this.cellWidth)
-    const lineIndex = Math.floor(y / this.cellheight)
     if (
-      lineIndex < 0 ||
-      cellIndex < 0 ||
-      lineIndex > this.grid2D.length ||
-      cellIndex > this.grid2D[0].length
+      x < 0 ||
+      y < 0 ||
+      y > this.grid2D.length ||
+      x > this.grid2D[0].length
     )
       return true
-    return this.grid2D[lineIndex][cellIndex]
+    console.log(' this.grid2D[y][x]',  this.grid2D[y][x]);
+    return this.grid2D[y][x]
   }
 
   runGameLoop() {
-    this.gameInterval = setInterval(() => {
+    // this.gameInterval = setInterval(() => {
       this.clearCanvas()
       this.drawMaze()
-    }, this.frameRate)
+    // }, this.frameRate)
   }
   clearCanvas(){
     ctx.clearRect(0, 0, canvasWidth, canvasHeight)
