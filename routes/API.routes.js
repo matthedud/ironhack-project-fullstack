bb0d9ea2c12ba5fba8558c9ce7a681b6b0029a3e
Matt K <matthedud@hotmail.com>
2022-03-29 14:19:38 +0200

    api setup

---
+++ routes/API.routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router()
+const express = require("express")
+const Map = require("../models/Map.model")
+const Historique = require("../models/Historic.model")
+
+router.get("/game", async (req, res, next) => {
+  try {
+    const map = await Map.findOne({ current: true })
+    if (map) {
+      const historics = await Historique.find({ map: map._id })
+      res.send({ map, historics })
+    } else {
+      const newMap = await Map.create()
+      res.send({ map: newMap, historics: [] })
+    }
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
+
+module.exports = router
