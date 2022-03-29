33e332fec497957094de26ef678d669c174a5794
Matt K <matthedud@hotmail.com>
2022-03-29 10:16:42 +0200

    map generator method

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 require("dotenv/config");
-
 require("./db");
-
-const express = require("express");
-
 const hbs = require("hbs");
 
+const express = require("express");
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require("./config")(app);
-
 require("./config/session.config")(app);
 
-// default value for title local
 const projectName = "lab-express-basic-auth";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
-// 👇 Start handling routes here
-
 const index = require("./routes/index");
 const authRouter = require("./routes/auth.routes");
 app.use("/", index);
 app.use("/auth/", authRouter);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
--- models/Map.model.js
+++ models/Map.model.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const mapSchema = new Schema({
   cells:{
      type: [[Number]]
@@ -16,6 +15,64 @@ const mapSchema = new Schema({
     default: true
   }
 });
+
+mapSchema.methods.createMap = async function( dimensions = 20,  maxTunnels = 50, maxLength = 8){
+      const map = createArray(1, dimensions) // create a 2d array full of 1's
+      let currentRow = Math.floor(Math.random() * dimensions) // our current row - start at a random spot
+      let currentColumn = Math.floor(Math.random() * dimensions) // our current column - start at a random spot
+      let directions = [[-1, 0], [1, 0], [0, -1], [0, 1]] // array to get a random direction from (left,right,up,down)
+      let lastDirection = [] // save the last direction we went
+      let randomDirection; // next turn/direction - holds a value from directions
+
+    // lets create some tunnels - while maxTunnels, dimentions, and maxLength  is greater than 0.
+    while (maxTunnels && dimensions && maxLength) {
+
+      // lets get a random direction - until it is a perpendicular to our lastDirection
+      // if the last direction = left or right,
+      // then our new direction has to be up or down,
+      // and vice versa
+      do {
+         randomDirection = directions[Math.floor(Math.random() * directions.length)];
+      } while ((randomDirection[0] === -lastDirection[0] && randomDirection[1] === -lastDirection[1]) || (randomDirection[0] === lastDirection[0] && randomDirection[1] === lastDirection[1]));
+
+      var randomLength = Math.ceil(Math.random() * maxLength), //length the next tunnel will be (max of maxLength)
+        tunnelLength = 0; //current length of tunnel being created
+
+		// lets loop until our tunnel is long enough or until we hit an edge
+      while (tunnelLength < randomLength) {
+
+        //break the loop if it is going out of the map
+        if (((currentRow === 0) && (randomDirection[0] === -1)) ||
+            ((currentColumn === 0) && (randomDirection[1] === -1)) ||
+            ((currentRow === dimensions - 1) && (randomDirection[0] === 1)) ||
+            ((currentColumn === dimensions - 1) && (randomDirection[1] === 1))) {
+          break;
+        } else {
+          map[currentRow][currentColumn] = 0; //set the value of the index in map to 0 (a tunnel, making it one longer)
+          currentRow += randomDirection[0]; //add the value from randomDirection to row and col (-1, 0, or 1) to update our location
+          currentColumn += randomDirection[1];
+          tunnelLength++; //the tunnel is now one longer, so lets increment that variable
+        }
+      }
+
+      if (tunnelLength) { // update our variables unless our last loop broke before we made any part of a tunnel
+        lastDirection = randomDirection; //set lastDirection, so we can remember what way we went
+        maxTunnels--; // we created a whole tunnel so lets decrement how many we have left to create
+      }
+    }
+    return map; // all our tunnels have been created and our map is complete, so lets return it to our render()
+}
+
+function createArray(num, dimensions) {
+  var array = [];
+  for (var i = 0; i < dimensions; i++) {
+    array.push([]);
+    for (var j = 0; j < dimensions; j++) {
+      array[i].push(num);
+    }
+  }
+  return array;
+}
 
 const Map = model("Map", mapSchema);
 
