af3e918c8cb3d54124d5821329d92856557e7d85
Matt K <matthedud@hotmail.com>
2022-03-31 09:59:58 +0200

    format

--- routes/API.routes.js
+++ routes/API.routes.js
@@ -6,55 +6,54 @@ const User = require("../models/User.model")
 
 router.get("/game", async (req, res, next) => {
   try {
-    const map = await Map.findOne({ current: true, isPublic:true });
-    const user = req.session?.user;
+    const map = await Map.findOne({ current: true, isPublic: true })
+    const user = req.session?.user
     if (map) {
-      console.log('got map');
       const timeElapse = new Date() - new Date(map.debut)
-      if(timeElapse<map.gameDuration){
-      console.log('time good');
-      const historics = await Historic.find({ map: map._id })
+      if (timeElapse < map.gameDuration) {
+        const historics = await Historic.find({ map: map._id })
         res.send({ map, historics, user })
         return
-      }
-      else{
-      console.log('time no good');
-      map.current = false
+      } else {
+        map.current = false
         await map.save()
       }
     }
-      console.log('create Map');
-      const newGrid = await Map.createMap()
-    const newMap = await Map.create({ cells: newGrid})
-    res.send({map:newMap, historics:[], user})
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-
-router.post("/game", async (req, res, next) => {
-  const {historic, ranking, historicBullets} = req.body
-  try {
-     await Historic.create( historic)
-     const savedMap = await Map.findByIdAndUpdate(historic.map, {ranking, historicBullets}, {new:true})
-     res.send(savedMap.ranking)
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-
-router.post("/map", isLoggedIn, async (req, res, next) => {
-  const {mapToSend} = req.body
-  try {
-    const newMap = await Map.create(mapToSend)
-    await User.findByIdAndUpdate(req.session.user._id,{$push: {"maps": newMap._id}})
-    res.send('/'+newMap._id)
+    const newGrid = await Map.createMap()
+    const newMap = await Map.create({ cells: newGrid })
+    res.send({ map: newMap, historics: [], user })
   } catch (error) {
     console.error(error)
     next(error)
-  } 
+  }
+})
+
+router.post("/game", async (req, res, next) => {
+  const { historic, ranking, historicBullets } = req.body
+  try {
+    await Historic.create(historic)
+    const savedMap = await Map.findByIdAndUpdate(
+      historic.map,
+      { ranking, historicBullets },
+      { new: true }
+    )
+    res.send(savedMap.ranking)
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
+
+router.post("/map", isLoggedIn, async (req, res, next) => {
+  const { mapToSend } = req.body
+  try {
+    const newMap = await Map.create(mapToSend)
+    await User.findByIdAndUpdate(req.session.user._id, { $push: { maps: newMap._id } })
+    res.send("/" + newMap._id)
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
 })
 
 module.exports = router
